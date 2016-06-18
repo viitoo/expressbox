754499a79335eded1ba1d72404ebe23fb21ba326
Victor Cedeno <nethax0r@gmail.com>
2016-06-18 19:54:25 -0400

    guardando cambios

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'byebug'
+
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
   devise_for :users
   get 'home/index'
 
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
