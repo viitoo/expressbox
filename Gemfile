b397fbb3b3a4daa5eb050c0d8a68b699eaee42ac
Victor Cedeno <nethax0r@gmail.com>
2016-07-28 09:09:29 -0400

    guardando antes de implementar upload files

--- Gemfile
+++ Gemfile
@@ -14,7 +14,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'humanizer'
 gem 'puma'
-
+#gem 'carrierwave', github: 'carrierwaveuploader/carrierwave'
+#gem 'mini_magick'
+gem "paperclip", "~> 5.0.0"
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/models/prealert.rb
+++ app/models/prealert.rb
@@ -1,8 +1,8 @@
 class Prealert < ActiveRecord::Base
-
-
+  has_attached_file :image, styles => {large: "600x600",medium: "300x300", thumb: "150x150"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   before_save :box_number
-
+  #mount_uploaders :avatars, AvatarUploader
 
 
   def box_number
