e0593eb35e83106e015a5d6d35c10988672734ed
Josh Adams <josh.rubyist@gmail.com>
2016-08-21 23:48:33 -0400

    saving crazy jquery admin.css and application.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require animations
 //= require bootstrap.min
 //= require custom
@@ -23,6 +26,3 @@
 //= require owl.carousel
 //= require retina
 //= require waypoints.min
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
