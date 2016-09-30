7114d5530ab0b2f89c7b7c1925b3d2d13e72ca9f
kw0lf <nethax0r@gmail.com>
2016-09-30 18:55:21 -0400

    saving after hour

--- app/controllers/box_controller.rb
+++ app/controllers/box_controller.rb
@@ -1,5 +1,6 @@
 class BoxController < ApplicationController
   before_action :authenticate_user!
+  layout "box"
   def index
     @prealerts = Prealert.where(user_id: current_user.id).order("updated_at DESC")
     @ship = Ship.last
